3a9fee59dc9e6e7f626835506909bc8a60d72c3c
thalsi <thalseehthalsi609@gmail.com>
2022-01-03 11:15:57 +0530

    ngModuel practcies

---
+++ src/app/practices/1-ngModule/ngMoulePractiveRouting.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { Router, RouterModule, Routes } from "@angular/router";
+
+const router:Routes= []
+
+@NgModule({
+    imports:[RouterModule.forChild(router)],
+    exports:[]
+})
+export class ngMoulePractiveRoutingMoulde { }
