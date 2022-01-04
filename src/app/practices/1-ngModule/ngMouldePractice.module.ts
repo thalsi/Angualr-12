ffdd24bd7a5ced27d9a79c237fa92872cf2b1177
thalsi <thalseehthalsi609@gmail.com>
2022-01-04 22:16:17 +0530

    ngModule

--- src/app/practices/1-ngModule/ngMouldePractice.module.ts
+++ src/app/practices/1-ngModule/ngMouldePractice.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ngMoulePractiveRoutingMoulde } from "./ngMoulePractiveRouting.module";
 
 @NgModule({
     declarations:[],
     imports:[
         CommonModule,
+        ngMoulePractiveRoutingMoulde
     ]
 })
 export class ngMoulePracticeModule {}
---
+++ src/app/practices/2-ngModule/ngModulePractice.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ngModulePractceRouting } from "./ngMouldePractceRouting.module";
+
+@NgModule({
+    declarations:[
+    ],
+    imports:[
+        CommonModule,
+        ngModulePractceRouting
+    ]
+})
+export class ngModulePractice {}
