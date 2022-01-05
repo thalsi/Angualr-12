54230f978e27adb3e23b54ec13a5c316782e5b0a
thalsi <thalseehthalsi609@gmail.com>
2022-01-05 09:48:23 +0530

    ngModule practices

---
+++ src/app/practices/3-ngModule/ngModulePracticeRouting.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const router:Routes =[];
+
+@NgModule({
+    imports:[
+        RouterModule.forChild(router)
+    ],
+    exports:[]
+})
+export class NgmodulePracticeRoutingModule {}
