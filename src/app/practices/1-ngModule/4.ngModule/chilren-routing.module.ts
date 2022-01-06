d276386cd2119a68298347e03d4fa4a0b703928b
thalsi <thalseehthalsi609@gmail.com>
2022-01-06 09:30:22 +0530

    ng module practices

---
+++ src/app/practices/1-ngModule/4.ngModule/chilren-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const router:Routes = [];
+
+@NgModule({
+    imports:[
+        RouterModule.forChild(router)
+    ],
+    exports:[]
+})
+
+export class ChilrenModuleRoutingModule { }
